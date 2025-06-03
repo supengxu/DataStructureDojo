9b172f604f50b71160109581d71bb726056f5a1a
xusupeng <xusupeng@beisen.com>
2025-06-03 19:04:36 +0800

    feat(LeetCode): 添加电话号码的字母组合问题解决方案
    - 新增 letterCombinationsOfAPhoneNumber_17.java 文件
    - 实现了电话号码字母组合的生成算法
    - 解决了 LeetCode 上的 [17] 电话号码的字母组合问题

---
+++ src/main/java/LeetCode/letterCombinationsOfAPhoneNumber_17.java
@@ -0,0 +1,95 @@
+/*
+ * @lc app=leetcode.cn id=17 lang=java
+ *
+ * [17] 电话号码的字母组合
+ *
+ * https://leetcode.cn/problems/letter-combinations-of-a-phone-number/description/
+ *
+ * algorithms
+ * Medium (62.19%)
+ * Likes:    3079
+ * Dislikes: 0
+ * Total Accepted:    1.1M
+ * Total Submissions: 1.8M
+ * Testcase Example:  '"23"'
+ *
+ * 给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。答案可以按 任意顺序 返回。
+ * 
+ * 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：digits = "23"
+ * 输出：["ad","ae","af","bd","be","bf","cd","ce","cf"]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：digits = ""
+ * 输出：[]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：digits = "2"
+ * 输出：["a","b","c"]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 0 <= digits.length <= 4
+ * digits[i] 是范围 ['2', '9'] 的一个数字。
+ * 
+ * 
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+// @lc code=start
+class Solution {
+    private String letterMap[] = {
+            " ",    //0
+            "",     //1
+            "abc",  //2
+            "def",  //3
+            "ghi",  //4
+            "jkl",  //5
+            "mno",  //6
+            "pqrs", //7
+            "tuv",  //8
+            "wxyz"  //9
+    };
+    public List<String> letterCombinations(String digits) {
+        var res = new ArrayList<String>();
+        if(digits.isEmpty())
+            return res;
+
+        findCombinations(res,0,"",digits);
+        return res;
+    }
+
+    private void findCombinations(ArrayList<String> res, int index, String s, String digits) {
+        if (index == digits.length()){
+            res.add(s);
+            return;
+        }
+        String letters = letterMap[digits.charAt(index) - '0'];
+        for (int i = 0; i < letters.length(); i++) {
+           findCombinations(res, index + 1, s + letters.charAt(i), digits);
+        }
+    }
+
+}
+// @lc code=end
+
