3d887a8f7bf896e8871919148b37d890b2a8564e
xusupeng <xusupeng@beisen.com>
2025-06-03 16:32:12 +0800

    refactor(LeetCode): 优化 3Sum 问题的解决方案- 移除了暴力三重循环的实现方式 - 对数组进行排序，提高查找效率 - 使用双指针法进行查找，降低时间复杂度 - 优化了去重逻辑，避免了不必要的重复计算

--- src/main/java/LeetCode/3Sum_15.java
+++ src/main/java/LeetCode/3Sum_15.java
@@ -15,16 +15,16 @@
  *
  * 给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j !=
  * k ，同时还满足 nums[i] + nums[j] + nums[k] == 0 。请你返回所有和为 0 且不重复的三元组。
- * 
+ *
  * 注意：答案中不可以包含重复的三元组。
- * 
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
+ *
  * 示例 1：
- * 
- * 
+ *
+ *
  * 输入：nums = [-1,0,1,2,-1,-4]
  * 输出：[[-1,-1,2],[-1,0,1]]
  * 解释：
@@ -33,33 +33,33 @@
  * nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0 。
  * 不同的三元组是 [-1,0,1] 和 [-1,-1,2] 。
  * 注意，输出的顺序和三元组的顺序并不重要。
- * 
- * 
+ *
+ *
  * 示例 2：
- * 
- * 
+ *
+ *
  * 输入：nums = [0,1,1]
  * 输出：[]
  * 解释：唯一可能的三元组和不为 0 。
- * 
- * 
+ *
+ *
  * 示例 3：
- * 
- * 
+ *
+ *
  * 输入：nums = [0,0,0]
  * 输出：[[0,0,0]]
  * 解释：唯一可能的三元组和为 0 。
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * 提示：
- * 
- * 
+ *
+ *
  * 3 <= nums.length <= 3000
  * -10^5 <= nums[i] <= 10^5
- * 
- * 
+ *
+ *
  */
 
 import java.util.ArrayList;
@@ -70,37 +70,69 @@ import java.util.stream.Collectors;
 
 // @lc code=start
 class Solution {
+    //    public List<List<Integer>> threeSum(int[] nums) {
+//        List<List<Integer>> res = new ArrayList<>();
+//        int n = nums.length;
+//
+//        // 使用 HashSet 来去重
+//        HashSet<String> seen = new HashSet<>();
+//
+//        // 暴力三重循环
+//        for (int i = 0; i < n; i++) {
+//            for (int j = 0; j < n; j++) {
+//                for (int k = 0; k < n; k++) {
+//                    // 确保三个索引互不相同
+//                    if (i != j && i != k && j != k) {
+//                        if (nums[i] + nums[j] + nums[k] == 0) {
+//                            // 排序后生成 key，用于去重
+//                            Integer[] triplet = new Integer[]{nums[i], nums[j], nums[k]};
+//                            Arrays.sort(triplet);
+//                            String key = Arrays.toString(triplet);
+//
+//                            if (!seen.contains(key)) {
+//                                seen.add(key);
+//                                res.add(Arrays.asList(triplet));
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//
+//        return res;
+//    }
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        int n = nums.length;
-
+        int len = nums.length;
+        // 先对数组排个序
+        Arrays.sort(nums);
         // 使用 HashSet 来去重
         HashSet<String> seen = new HashSet<>();
 
         // 暴力三重循环
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                for (int k = 0; k < n; k++) {
-                    // 确保三个索引互不相同
-                    if (i != j && i != k && j != k) {
-                        if (nums[i] + nums[j] + nums[k] == 0) {
-                            // 排序后生成 key，用于去重
-                            Integer[] triplet = new Integer[]{nums[i], nums[j], nums[k]};
-                            Arrays.sort(triplet);
-                            String key = Arrays.toString(triplet);
+        for (int i = 0; i < len; i++) {
+            if (nums[i] > 0) break;
 
-                            if (!seen.contains(key)) {
-                                seen.add(key);
-                                res.add(Arrays.asList(triplet));
-                            }
-                        }
-                    }
-                }
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // 去重
+
+            int L = i + 1;
+            int R = len - 1;
+            while (L < R) {
+                int sum = nums[i] + nums[L] + nums[R];
+                if (sum == 0) {
+                    res.add(Arrays.asList(nums[i], nums[L], nums[R]));
+                    while (L < R && nums[L] == nums[L + 1]) L++; // 去重
+                    while (L < R && nums[R] == nums[R - 1]) R--; // 去重
+                    L++;
+                    R--;
+                } else if (sum < 0) L++;
+                else if (sum > 0) R--;
             }
         }
 
         return res;
     }
+
 }
 // @lc code=end
 
