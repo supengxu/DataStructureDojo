06e6445d991f4766ec616d88173db05f51ec3d4c
xusupeng <xusupeng@beisen.com>
2025-06-03 16:20:20 +0800

    feat(LeetCode): 添加 3Sum 问题的解决方案
    - 实现了三数之和问题的暴力解法
    - 使用 HashSet进行结果去重
    -通过排序和字符串表示来进一步去重

---
+++ src/main/java/LeetCode/3Sum_15.java
@@ -0,0 +1,106 @@
+/*
+ * @lc app=leetcode.cn id=15 lang=java
+ *
+ * [15] 三数之和
+ *
+ * https://leetcode.cn/problems/3sum/description/
+ *
+ * algorithms
+ * Medium (39.50%)
+ * Likes:    7499
+ * Dislikes: 0
+ * Total Accepted:    2.3M
+ * Total Submissions: 5.9M
+ * Testcase Example:  '[-1,0,1,2,-1,-4]'
+ *
+ * 给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j !=
+ * k ，同时还满足 nums[i] + nums[j] + nums[k] == 0 。请你返回所有和为 0 且不重复的三元组。
+ * 
+ * 注意：答案中不可以包含重复的三元组。
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：nums = [-1,0,1,2,-1,-4]
+ * 输出：[[-1,-1,2],[-1,0,1]]
+ * 解释：
+ * nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0 。
+ * nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0 。
+ * nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0 。
+ * 不同的三元组是 [-1,0,1] 和 [-1,-1,2] 。
+ * 注意，输出的顺序和三元组的顺序并不重要。
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：nums = [0,1,1]
+ * 输出：[]
+ * 解释：唯一可能的三元组和不为 0 。
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：nums = [0,0,0]
+ * 输出：[[0,0,0]]
+ * 解释：唯一可能的三元组和为 0 。
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 3 <= nums.length <= 3000
+ * -10^5 <= nums[i] <= 10^5
+ * 
+ * 
+ */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
+// @lc code=start
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        int n = nums.length;
+
+        // 使用 HashSet 来去重
+        HashSet<String> seen = new HashSet<>();
+
+        // 暴力三重循环
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    // 确保三个索引互不相同
+                    if (i != j && i != k && j != k) {
+                        if (nums[i] + nums[j] + nums[k] == 0) {
+                            // 排序后生成 key，用于去重
+                            Integer[] triplet = new Integer[]{nums[i], nums[j], nums[k]};
+                            Arrays.sort(triplet);
+                            String key = Arrays.toString(triplet);
+
+                            if (!seen.contains(key)) {
+                                seen.add(key);
+                                res.add(Arrays.asList(triplet));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return res;
+    }
+}
+// @lc code=end
+
